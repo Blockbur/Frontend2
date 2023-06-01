0e4c0eb7d22d2599366e56cdabe399320a7a120d
Davi <88855441+davi12345452@users.noreply.github.com>
2023-06-01 19:02:04 -0300

    Update functions.ts
    mainet

--- src/contract/functions.ts
+++ src/contract/functions.ts
@@ -13,7 +13,7 @@ async function convertFromBigIntToInteger(number: any) {
 
 export async function instantiteContractWithRpcUrl() {
   const provider = new ethers.JsonRpcProvider(
-    'https://polygon-mumbai.g.alchemy.com/v2/6XFcpiY-OYMhStUfVvVuYdPUMyeQOZbW',
+    "https://polygon-mainnet.g.alchemy.com/v2/na34V2wPZksuxFnkFxeebWVexYWG_SnR",
   )
 
   const contractInstance = new ethers.Contract(
